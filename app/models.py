c832ee5d6b8c40d1358e85660a1c2167b0b84fde
Julius <juliusalphonso.09@gmail.com>
2019-09-20 01:35:27 +0530

    Add gravatar for user

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
+from hashlib import md5
 
 @login.user_loader
 def load_user(id):
@@ -44,6 +45,10 @@ class User(UserMixin, db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
     def __repr__(self):
         return f'<User {self.username}>'
