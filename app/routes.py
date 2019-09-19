69c51a15f33b1aa62ccc8ff1e39d13a177f14b7d
Julius <juliusalphonso.09@gmail.com>
2019-09-19 18:52:03 +0530

    Setup User Login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 user = {
     'username': 'Shawn'
@@ -9,6 +11,15 @@ user = {
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/leaderboard')
 def leaderboard():
