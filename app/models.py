0c7488e5cd03061e71c6b96b9bcbd98146165dc2
Julius <juliusalphonso.09@gmail.com>
2019-09-19 20:43:10 +0530

    Extend Users Schema to include profile info

--- app/models.py
+++ app/models.py
@@ -4,12 +4,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
+    # Login details
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(32, index=False, unique=False))
-    last_name = db.Column(db.String(32, index=False, unique=False))
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    # Profile details
+    first_name = db.Column(db.String(32))
+    last_name = db.Column(db.String(32))
+    college = db.Column(db.String(64))
+    website = db.Column(db.String(120))
+    about_me = db.Column(db.Text)
+    social_github  = db.Column(db.String(120))
+    social_linked  = db.Column(db.String(120))
+    social_twitter = db.Column(db.String(120))
+    # Projects
+
+    # Upvotes
+    upvotes = db.relationship('Post', backref='user', lazy='dynamic') 
+    # Comments
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
